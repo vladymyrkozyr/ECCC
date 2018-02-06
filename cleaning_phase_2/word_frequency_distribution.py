5ce5905950c888bf1704d939b9a3ecfafc0b619c
Chelsea (Chao) Li <chelseachaoli@gmail.com>
2018-02-05 21:00:36 -0800

    output csv files for cleaned data
    This update creates a new folder named "filtered_data" under each social media folder to save new csv files.
    The old "word_count" folders under "cleaned data" can be deleted now.
    Please run the script on all file. Thank you Vova and Bdour!

--- cleaning_phase_2/word_frequency_distribution.py
+++ cleaning_phase_2/word_frequency_distribution.py
@@ -16,22 +16,18 @@ from nltk.probability import FreqDist
 ### Choose a social media file path
 
 #======= Twitter =======
-#rootPath='../tw/cleaned data/*.csv'
-#COLUMN_NAME = 'full_text_cleaned'
+rootPath='../tw/cleaned data/*.csv'
+COLUMN_NAME = 'full_text_cleaned'
 
 #======= Facebook =======
-#rootPath='../fb/cleaned data/statuses/*.csv'
-#COLUMN_NAME = 'status_message_cleaned'
+#rootPath='../fb/cleaned data/comments/*.csv'
+#COLUMN_NAME = 'comment_message_cleaned'
 
 #======= Instagram =======
-rootPath='../in/cleaned data/comments/*.csv'
-COLUMN_NAME = 'text_cleaned'
+#rootPath='../in/cleaned data/posts/*.csv'
+#COLUMN_NAME = 'caption'
 
-
-outputDir = rootPath[:-5] + 'word_count/'
-if not os.path.exists(outputDir):
-    os.makedirs(outputDir)
-
+filePaths = glob.glob(rootPath)  
 multiWordsPath = './multiwords.txt'
 
 
@@ -65,7 +61,7 @@ def load_multi_words(filepath):
 
 def tokenize_multi_words(topic_list):
     result = []
-    print('>>>Adding custermized topic words/tokens to tokenizer...')
+    print('>>> Adding custermized topic words/tokens to tokenizer...')
     for words in topic_list:
         print(words)
         result.append(words.split('_'))
@@ -95,18 +91,22 @@ def stem_text(text):
 
 def main():
     filePaths = glob.glob(rootPath)  
+    wordCountOutputDir = os.path.dirname(rootPath).replace('cleaned data', 'filtered_data') + '/word_count/'
+    if not os.path.exists(wordCountOutputDir):
+        os.makedirs(wordCountOutputDir)  
+
     for filepath in filePaths:
         filename = os.path.basename(filepath)
-        outputFileName = filename[:-4] + '_word_count.csv'
+        cleanedDataFileName = filepath.replace('cleaned data', 'filtered_data')
+        wordCountFileName = wordCountOutputDir + os.path.splitext(filename)[0] + '_word_count.csv'
         print('>>> Processing file: ' + filename)
 
         df = pd.read_csv(filepath)   # the first unnamed column already exists in csv file 
         df['text_filtered'] = df[COLUMN_NAME].astype(str).apply(filter_stop_words)
-
-
         df['text_tokenized'] = df['text_filtered'].apply(tokenize_text)
         df['text_stemmed'] = df['text_tokenized'].apply(stem_text)
-
+        print('>>> Output cleaned data for ' + filename)
+        df.to_csv(cleanedDataFileName, index=None) 
 
         token_lst = df['text_tokenized'].tolist()
         token_fdist = FreqDist()
@@ -115,14 +115,12 @@ def main():
             for token in list_i:
                 token_fdist[token.lower()] += 1
 
-
         stemmer_lst = df['text_stemmed'].tolist()
         stemmer_fdist = FreqDist()
         for list_i in stemmer_lst:
             list_i = set(list_i)  # Adding this line would count a word once even if it appears multple times in one comment/post
             for token in list_i:
                 stemmer_fdist[token.lower()] += 1
-
 
         # ## Output token/stemmer frequency distribution
         token_df = pd.DataFrame(list(token_fdist.items()), columns=['token', 'tok_freq'])
@@ -135,7 +133,8 @@ def main():
 
         print('>>> Output word frequency distribution for ' + filename)
         output_df = pd.concat([token_df, stemmer_df], axis=1)
-        output_df.to_csv(outputDir + outputFileName, index=None)      
+        output_df.to_csv(wordCountFileName, index=None)   
+        output_df      
 
 
 if __name__ == '__main__':

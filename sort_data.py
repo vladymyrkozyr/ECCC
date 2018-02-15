b3ac2594c52ad0c47017b1c5bd5a94a9c0211cbc
root <root@DOPESMOKER.localdomain>
2018-02-15 15:47:07 -0800

    categorized posts. made word lists lowercase

--- sort_data.py
+++ sort_data.py
@@ -4,66 +4,124 @@ import pandas
 import glob
 import os
 import sys
+from textblob import TextBlob
+import nltk
+from nltk.stem.snowball import FrenchStemmer
+from nltk.stem.snowball import EnglishStemmer
+from langdetect import detect
+from operator import itemgetter
+import six
 reload(sys)
 sys.setdefaultencoding('utf8')
-from textblob import TextBlob
+
+def detect_lang(text):
+    try:
+        lang = detect(text)
+    except:
+        return 'error'
+    return lang
 
 
-##for twitter
+def stem_text(text):
+    if detect_lang(text) == 'fr':
+        stemmer = FrenchStemmer()
+    else:
+        stemmer = EnglishStemmer() 
+    return stemmer.stem(text)
+   # stems = [stemmer.stem(tok) for tok in text]
+   # return stems
+
+
+
+#for twitter
 #rootPath='tw/filtered_data_spell_corrected/*.csv'
-#rootResultPath='tw/filtered_data_spell_corrected/'
+#rootResultPath_tagged='tw/filtered_data_spell_corrected/'
 
 #for facebook
-rootPath='fb/filtered_data_spell_corrected/statuses/*.csv'
-rootResultPath='fb/filtered_data_spell_corrected/comments/'
+#social_network='facebook'
+#rootPath='fb/filtered_data_spell_corrected/statuses/*.csv'
+#rootResultPath_tagged='fb/filtered_data_spell_corrected/statuses/'
 
 #for instagram
-#rootPath='in/filtered_data_spell_corrected/posts/*.csv'
-#rootResultPath='in/filtered_data_spell_corrected/comments/'
+rootPath='in/filtered_data_spell_corrected/posts/*.csv'
+rootResultPath_tagged='in/filtered_data_spell_corrected/posts/'
 
 files = glob.glob(rootPath)  
 
 names = [os.path.basename(x) for x in glob.glob(rootPath)]
 
-keywords_filePath='Keywords_ECCC.csv'
+keywords_filePath_EN='Keywords_ECCC_EN.csv'
+keywords_filePath_FR='Keywords_ECCC_FR.csv'
 
-keywords=pandas.read_csv(keywords_filePath)
+keywords=pandas.concat([pandas.read_csv(keywords_filePath_EN,engine='python'),pandas.read_csv(keywords_filePath_FR,engine='python')])
+stemmed_keywords=keywords
 
-print keywords
+social_column=[]
+economical_column=[]
+environmental_column=[]
+
+#for soc, eco, env in zip(keywords['Social'],keywords['Economical'],keywords['Environmental']):
+#    social_column.append(stem_text((soc)))
+#    economical_column.append(stem_text((eco)))
+#    environmental_column.append(stem_text((env)))
+
+#print social_column
+#print economical_column
+#print environmental_column
+#print len(social_column)
+#print len(economical_column)
+#print len(environmental_column)
+stemmed_keywords['Social']=keywords['Social']
+stemmed_keywords['Economical']=keywords['Economical']
+stemmed_keywords['Environmental']=keywords['Environmental']
+
+
+#print keywords
+#
+#print "adsfdfasdfdafadf"
+#
+#print stemmed_keywords
 
 
 for file, name in zip(files, names):
     info=pandas.read_csv(file)
     
     print name
-    print
+    print 
+         
+    categories=[]
     for post in info['text_original']:
-        print str(post).lower()
+        #print str(post).lower()
         social=0
         economical=0
-        environmental=0
-        for social_word, economical_word, environmental_word in zip(keywords['Social'],keywords['Economical'],keywords['Environmental']):
+        environmental=0   
+        for social_word, economical_word, environmental_word in zip(stemmed_keywords['Social'],stemmed_keywords['Economical'],stemmed_keywords['Environmental']):
             
-#            print social_word
-#            print economical_word
-#            print environmental_word
-            if str(social_word).lower() in str(post).lower() and str(social_word).lower()!='nan':
-                social+=1
-            if str(economical_word).lower() in str(post).lower() and str(economical_word).lower()!='nan':
-                economical+=1
-            if str(environmental_word).lower() in str(post).lower() and str(environmental_word).lower()!='nan':
-                environmental+=1
+            p=str(post).lower()
+           # print type(economical_word)
+            #print (type(social_word)!='float')
+                        
+            if isinstance(social_word,six.string_types):
+                if social_word in p:
+                    social+=1
+            if isinstance(economical_word,six.string_types):
+                if economical_word in p:
+                    economical+=1
+            if isinstance(environmental_word,six.string_types):
+                if environmental_word in p:
+                    environmental+=1
         
-        print ('social: '+str(social))
-        print ('economical: '+str(economical))
-        print ('environmental: '+str(environmental))
-    break
-
-print 'started'
-
-#for file, name in zip(files, names):
-#    accountInfo=pandas.read_csv(file)
-#    print name
-#    print
-#    for text in accountInfo['text_original']:
-        
+        c=max([('Social',social),('Economical',economical),('Environmental',environmental)],key=itemgetter(1))[0]
+        m=max([('Social',social),('Economical',economical),('Environmental',environmental)],key=itemgetter(1))
+        if m==0:
+            categories.append('unknown')
+        else:
+            categories.append(c)
+            
+#        print c
+#        print social
+#        print economical        
+#        print environmental
+            
+    info['category']=categories
+    info.to_csv(rootResultPath_tagged+name)        

858729f4746a7f42eb94f020f78a37cfe1b6ef43
Chelsea (Chao) Li <chelseachaoli@gmail.com>
2018-02-23 14:21:38 -0800

    convert decimals to percentage format
    also train 4 models in one run, i.e. English/French X cleaned_text/raw_text

--- cleaning_phase_2/classify_topics_with_LDA.py
+++ cleaning_phase_2/classify_topics_with_LDA.py
@@ -16,10 +16,11 @@ from nltk.stem.wordnet import WordNetLemmatizer
 import gensim
 from gensim import corpora
 import string
+from numbers import Number
 from pprint import pprint
 import logging
 
-pd.options.display.max_rows = 20
+pd.options.display.max_rows = 10
 
 
 # In[2]:
@@ -73,15 +74,27 @@ def detect_lang(text):
 
 
 def normalize_text(row):   
-    text = row['cleaned_text']
-    stopWords = stopWords_en  
+    stopWords = stopWords_en 
     if row['lang'] == 'fr':
-        stopWords = stopWords_fr  
-    stop_free = ' '.join([w for w in wordpunct_tokenize(text) if w.lower() not in stopWords
-            and len(w) > 1 and w.isalnum()]) 
-    punc_free = ''.join(ch.lower() for ch in stop_free if ch not in exclude)
-    normalized = ' '.join(lemma.lemmatize(word) for word in punc_free.split())
-    return normalized.split()
+        stopWords = stopWords_fr 
+    text_cols = row['cleaned_text'], row['raw_text']
+    normalized_text = []
+    for text in text_cols:
+        #stop_free = ' '.join([w for w in wordpunct_tokenize(text) if w.lower() not in stopWords
+        #        and len(w) > 1 and w.isalnum()]) 
+        stop_free = ' '.join([w for w in wordpunct_tokenize(text) if w.lower() not in stopWords and len(w) > 1])
+        punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+        normalized = ' '.join(lemma.lemmatize(word).lower() for word in punc_free.split() if len(lemma.lemmatize(word)) > 2)
+        normalized = ' '.join(w for w in normalized.split() if w not in stopWords)
+        normalized_text.append(normalized.split())
+    return normalized_text[0], normalized_text[1]
+
+
+def as_percent(text, precision='0.2'):  
+    if isinstance(text, Number):
+        return "{{:{}%}}".format(precision).format(text)
+    else:
+        raise TypeError("Numeric type required")
 
 
 # In[ ]:
@@ -100,7 +113,7 @@ df_merge
 
 # Detect language of a post. Several languages other than english or french are dectecd but makes up less than 1%
 df_merge['lang'] = df_merge['cleaned_text'].apply(detect_lang)
-df_merge.groupby('lang').count()
+#df_merge.groupby('lang').count()
 
 
 # In[ ]:
@@ -108,48 +121,62 @@ df_merge.groupby('lang').count()
 
 # filter out exotic languages
 df_merge = df_merge[(df_merge['lang'] == 'en') | (df_merge['lang'] == 'fr')]
+#df_backup = df_merge.copy()
 
 
 # In[ ]:
 
 
 # pre-processing text for LDA
-df_merge['normalized_text'] = df_merge.apply(normalize_text, axis=1)
-df_merge['normalized_text']
+df_merge['normalized_cleaned_text'], df_merge['normalized_raw_text']  = zip(*df_merge.apply(normalize_text, axis=1))
+df_merge
+
+
+# In[ ]:
+
+
+#df_merge['normalized_cleaned_text'].tolist()
 
 
 # In[ ]:
 
 
 # train two LDA models, one for 'English', the other for 'French'
-for lang in ['en', 'fr']:
-    df_sub = df_merge[df_merge['lang'] == lang]
-    doc_clean = df_sub['normalized_text'].tolist() 
-    
-    # Creating the term dictionary of our courpus, where every unique term is assigned an index. 
-    dictionary = corpora.Dictionary(doc_clean)
-    dictionary.save('LDA_dictionary_' + lang + '.dict')  # store the dictionary, for future reference
-    
-    # Converting list of documents (corpus) into Document Term Matrix using dictionary prepared above.
-    corpus = [dictionary.doc2bow(doc) for doc in doc_clean]
-    corpora.MmCorpus.serialize('LDA_corpus_' + lang + '.mm', corpus)
-    pprint(len(dictionary.token2id))
-    
-    logging.basicConfig(filename='lda_model_' + lang + '.log', format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    
-    print('training model for ' + lang)
-    # Running and Training LDA model on the document term matrix.
-    ldamodel = gensim.models.ldamodel.LdaModel(corpus=corpus, num_topics=NUM_TOPICS, id2word=dictionary, passes=5)
-    
-    # save results
-    result = ldamodel.show_topics(num_topics=NUM_TOPICS, num_words=100, formatted=False)
-    df_concat = pd.DataFrame()
-    for i in range(0, NUM_TOPICS):
-        df_tmp = pd.DataFrame(result[i][1], columns=['#' + str(result[i][0]) + '_word', '#' + str(result[i][0]) + '_prob'])
-        df_concat = pd.concat([df_concat, df_tmp], axis=1)
-        #display(df_concat)
-    df_concat.to_csv('../LDA_classify_topics_' + lang + '.csv', index=None) 
+for lang in ['fr', 'en']:
+    for col in ['cleaned_text', 'raw_text']:
+        df_sub = df_merge[df_merge['lang'] == lang]
+        doc_clean = df_sub['normalized_' + col].tolist() 
+
+        # Creating the term dictionary of our courpus, where every unique term is assigned an index. 
+        dictionary = corpora.Dictionary(doc_clean)
+        dictionary.save('./LDA_files/LDA_dictionary_' + col + '_'+ lang + '.dict')  # store the dictionary, for future reference
+
+        # Converting list of documents (corpus) into Document Term Matrix using dictionary prepared above.
+        corpus = [dictionary.doc2bow(doc) for doc in doc_clean]
+        corpora.MmCorpus.serialize('./LDA_files/LDA_corpus_' + col + '_' + lang + '.mm', corpus)
+        pprint(len(dictionary.token2id))
+
+        logging.basicConfig(filename='./LDA_files/lda_model_' + col + '_' + lang + '.log',
+                            format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+        print('training model for ' + col + ' in ' + lang)
+        # Running and Training LDA model on the document term matrix.
+        ldamodel = gensim.models.ldamodel.LdaModel(corpus=corpus, num_topics=NUM_TOPICS, id2word=dictionary, passes=5)
+
+        # save results
+        result = ldamodel.show_topics(num_topics=NUM_TOPICS, num_words=200, formatted=False)
+        df_concat = pd.DataFrame()
+        for i in range(0, NUM_TOPICS):
+            df_tmp = pd.DataFrame(result[i][1], columns=['#' + str(result[i][0]) + '_word', '#' + str(result[i][0]) + '_prob'])
+            df_concat = pd.concat([df_concat, df_tmp], axis=1)
+            #display(df_concat)
+            
+        df_concat['#0_prob'] = df_concat['#0_prob'].apply(as_percent)
+        df_concat['#1_prob'] = df_concat['#1_prob'].apply(as_percent)
+        df_concat['#2_prob'] = df_concat['#2_prob'].apply(as_percent)
+        
+        df_concat.to_csv('../LDA_classify_topics_with_' + col + '_' + lang + '.csv', index=None) 
+        df_concat
 
 
 # In[ ]:

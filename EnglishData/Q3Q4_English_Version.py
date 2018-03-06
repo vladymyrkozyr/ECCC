87fc7133eb70e777fbbe9ed3d2757402751a6325
Chelsea (Chao) Li <chelseachaoli@gmail.com>
2018-03-06 12:50:16 -0800

    new version with tokenization

--- EnglishData/Q3Q4_English_Version.py
+++ EnglishData/Q3Q4_English_Version.py
@@ -1,99 +1,182 @@
-# -*- coding: utf-8 -*-
-import csv
-import pandas
+
+# coding: utf-8
+
+# In[1]:
+
+
 import glob
 import os
-import sys
-from textblob import TextBlob
-import nltk
-from nltk.stem.snowball import FrenchStemmer
-from nltk.stem.snowball import EnglishStemmer
+import pandas as pd
+
 from langdetect import detect
-from operator import itemgetter
-import six
-reload(sys)
-sys.setdefaultencoding('utf8')
+import nltk
+#nltk.download()   # comment after first download
+from nltk.tokenize import wordpunct_tokenize, MWETokenizer
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+import gensim
+from gensim import corpora
+import string
+from numbers import Number
+from pprint import pprint
+import logging
+import operator
+
+pd.options.display.max_rows = 30
 
 
-##for twitter
-rootPath='../question answers/question 3 and 4/tw/*.csv'
-rootResultPath_tagged='../question answers/question 3 and 4/tw/'
+# In[2]:
 
 
-##for facebook
-#rootPath='../question answers/question 3 and 4/fb/posts/*.csv'
-#rootResultPath_tagged='../question answers/question 3 and 4/fb/posts/'
+keywords_chosen = '13_FSDS_Goals_Keywords_EN.csv'
+
+data_folder = './Accounts/output_chelsea/*.csv'
+
+CSV_COLUMNS = ['caption_cleaned', 'hashtags']
 
 
-##for instagram
-#rootPath='../question answers/question 3 and 4/in/posts/*.csv'
-#rootResultPath_tagged='../question answers/question 3 and 4/in/posts/'
+# In[3]:
 
 
-files = glob.glob(rootPath) 
-
-names = [os.path.basename(x) for x in glob.glob(rootPath)]
-
-keywords_filePath_EN='../13_FSDS_Goals_Keywords_EN.csv'
-keywords_filePath_FR='../13_FSDS_Goals_Keywords_FR.csv'
-
-keywords=pandas.concat([pandas.read_csv(keywords_filePath_EN,engine='python'),pandas.read_csv(keywords_filePath_FR,engine='python')])
-stemmed_keywords=keywords
+# create output directory
+outputDir = os.path.dirname(data_folder).replace('output_chelsea', 'q3q4_output_chelsea') + '/'
+if not os.path.exists(outputDir):
+    os.makedirs(outputDir)
 
 
+# In[4]:
 
-for file, name in zip(files, names):
-    info=pandas.read_csv(file)
-    
-    print name
-    print 
-    
-    FSDS_Goal_Category=[]
-    FSDS_Goal_Category_word_list=[]
-    
-    for post in info['caption_cleaned']:
-                
-        list_of_tuples=[]
-        for column in stemmed_keywords:
-            score=0
-            word_list=[]
-            for word in stemmed_keywords[column]:                
-                p=str(post).lower()
-                if isinstance(word,six.string_types):
-                    if word in p:
-                        score+=1
-                        word_list.append(word)
-            list_of_tuples.append((str(column),score,word_list))
+
+# set of punctuations to remove from text
+exclude = set(string.punctuation)
+
+
+# In[5]:
+
+
+stopWords = set(stopwords.words('english'))
+stopWords.add('theyre')   # an informal spelling
+
+lemma = WordNetLemmatizer()    # NLTK English lemmatizer
+
+# detect_lang function can be use to check the percentage of non English posts
+# note that missing value NaN can be detected as many different languages such english, spanish or italian
+def detect_lang(text):
+    try:
+        lang = detect(text)
+    except:
+        return 'error'
+    return lang
+
+# lemmatize_keywords also clears 'nan' from input keyword list file
+# lemmatization is conducted based on context, some words may not get lemmatized, 
+# e.g. "local eating" does not get lemmatized to "local eat"
+def lemmatize_keywords(col):
+    if str(col).lower() == 'nan':
+        return ''
+    return '_'.join(lemma.lemmatize(word).lower() for word in col.replace('’', '\'').replace('.', '').split()) #
+
+
+# In[6]:
+
+
+# load keywords list
+pd.options.display.max_rows = 100
+keywords_df = pd.read_csv(keywords_chosen, encoding='latin-1')   # "ISO-8859-1"
+KEYWORDS_COLS = keywords_df.columns
+lemma_keywords_df = pd.DataFrame(columns=KEYWORDS_COLS)
+category_dict = {}
+keywords_list = set()
+for col in KEYWORDS_COLS:
+    lemma_keywords_df[col] = keywords_df[col].astype(str).apply(lemmatize_keywords)
+    category_dict[col] = set(lemma_keywords_df[col].tolist())
+    category_dict[col].remove('')
+    keywords_list = keywords_list.union(category_dict[col])
+
+
+# if there are punctuations in the keywords list, these punctuation will be kept regardless of puncturation removal step
+for word in keywords_list:
+    for char in word:
+        if char in exclude:
+            exclude.remove(char)
             
-            
-        c=max(list_of_tuples,key=itemgetter(1))[0]
-            
-        m=max(list_of_tuples,key=itemgetter(1))
-       
-        l=max(list_of_tuples,key=itemgetter(1))[2]
+# Add all words in the given keyword list to pre-defined token dictionary
+multi_word = [w.split('_') for w in keywords_list ]   #if '_' in w 
+tokenizer = MWETokenizer(multi_word)
 
-        if len(l)==0:
-            FSDS_Goal_Category.append('unknown')
-            FSDS_Goal_Category_word_list.append('')
-        else:
-            FSDS_Goal_Category.append(c)            
-            FSDS_Goal_Category_word_list.append(list(set(l)))
+
+# In[7]:
+
+
+def lemmatize_text(row): 
+    text = str(row['caption_original'])
+    #print(text)
+    text = text.replace('’', '\'')
+    tokens = tokenizer.tokenize(text.split())   
+    # remove stop words
+    stop_free = ' '.join(w for w in tokens if w not in stopWords and len(w) > 1)
+    # remove punctuation
+    punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+    # lemmatize
+    lemmas = ' '.join(lemma.lemmatize(word).lower() for word in punc_free.split() if len(lemma.lemmatize(word)) > 1)
+    # remove stop words that appear after lemmatization
+    stop_free_2 = ' '.join(w for w in lemmas.split() if w not in stopWords and len(w) > 1)
+    #print(stop_free_2)
+    return stop_free_2.split()
+
+# assign a category based the max number of keywords found in each category
+def find_category(row):
+    text = row['lemmatized_text']
+    keywords_found = []
+    counter = {}
+    for col in KEYWORDS_COLS:
+        counter[col] = 0
+    #print(counter)
+    category = 'unknown'
+    for word in text:
+        for col in KEYWORDS_COLS:
+            if word in category_dict[col]:
+                #print(word)
+                #print(category_dict[col])
+                keywords_found.append(word)
+                counter[col] += 1
+    if len(keywords_found) > 0:
+        category = max(counter.items(), key=operator.itemgetter(1))[0]    
+    return keywords_found, category
+
+
+# In[8]:
+
+
+pd.options.display.max_rows = 10
+# read csv files and save targt columns to dataframe
+filePaths = glob.glob(data_folder)  
+for filename in filePaths:
+    print(filename)
+    basename = os.path.basename(filename)
+    outputFileName = outputDir + basename
+    data_df = pd.read_csv(filename, encoding = 'utf-8')
+    if data_df.shape[0] < 1:
+        data_df.to_csv(outputFileName, index=None) 
+        continue
+    #data_df['lang'] = data_df['caption_cleaned'].astype(str).apply(detect_lang)
+    #data_df = data_df.drop(['category', 'words_matched_list'], axis=1)
+    data_df.fillna('')
+    #wrong_lang = data_df[data_df['lang'] != 'en'].shape[0]
+    try:
+        data_df['lemmatized_text'] = data_df.apply(lemmatize_text, axis=1)
+    except:
+        print('cannot process file: ' + basename)
+        continue
+    data_df['FSDS_matched_keywords'], data_df['FSDS_category'] = zip(*data_df.apply(find_category, axis=1))
+    output_list = data_df.columns.tolist()
+    output_list.remove('lemmatized_text')
+    output_df = data_df[output_list]
+    output_df.to_csv(outputFileName, index=None)    
     
 
-    
-    info['FSDS_Goal_Category']=FSDS_Goal_Category
-    info['FSDS_Goal_Category_word_list']=FSDS_Goal_Category_word_list
-    info.to_csv(rootResultPath_tagged+name)   
-    
-    # CLEAN UNNAMED COLUMNS
+# In[10]:
 
-for file, name in zip(files, names):
-    info=pandas.read_csv(file)
-    
-    
-    print name
-    print 
-                                   
-    info = info[info.columns.drop(list(info.filter(regex='Unnamed')))]
 
-    info.to_csv(rootResultPath_tagged+name)        
+print(exclude)
+

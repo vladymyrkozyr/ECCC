5318e932eafaeac71954f073a26dc3d2d7cae88a
brieh <brieh@sfu.ca>
2018-03-07 15:41:28 -0800

    updated to do a groupby to get number of unknowns by account_name

--- FrenchData/extract_unknown_posts.py
+++ FrenchData/extract_unknown_posts.py
@@ -2,10 +2,10 @@ import glob
 
 import pandas as pd
 
-
 pd.options.display.max_rows = 30
 
-data_folder = './ECCC/FrenchData/Accounts/Output/*.csv'
+data_folder = r'./Accounts/q1_output/*.csv'
+output_loc = r'allpostswithunknown.csv'
 
 CSV_COLUMNS = ['caption_original', 'category', 'account_name']
 
@@ -14,7 +14,9 @@ CSV_COLUMNS = ['caption_original', 'category', 'account_name']
 pd.options.display.max_rows = 1000
 # read csv files and save targt columns to dataframe
 filePaths = glob.glob(data_folder)
+
 data_df = pd.DataFrame()
+print(filePaths)
 for f in filePaths:
     print(f)
     df = pd.read_csv(f, encoding='utf-8')
@@ -23,4 +25,8 @@ for f in filePaths:
     df = df[CSV_COLUMNS]
     data_df = pd.concat([data_df, df])
 data_df = data_df.loc[data_df['category'] == 'unknown']
-data_df.to_csv('allpostswithUnknown.csv', index=CSV_COLUMNS)
+data_df.to_csv(output_loc, index=CSV_COLUMNS)
+
+#get number of unknowns by account_name
+result = data_df.groupby(['account_name']).size()
+print(result)
